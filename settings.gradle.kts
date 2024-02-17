88778150deaf675540119a824b2fc0b96dee0fb5
gta61 <90571440+gta61@users.noreply.github.com>
2024-02-17 03:15:24 +0100

    Start with Neumorphism

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") } // Corrected line #
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") } // Corrected line #
+    }
+}
+
+rootProject.name = "Brutalarm"
+include(":app")
