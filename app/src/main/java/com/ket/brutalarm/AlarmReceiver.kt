93abc494c50b85f3641884e4b7130f879207a28d
gta61 <90571440+gta61@users.noreply.github.com>
2024-02-21 16:35:36 +0100

    added doc comment for  AlarmReceiver.kt

--- app/src/main/java/com/ket/brutalarm/AlarmReceiver.kt
+++ app/src/main/java/com/ket/brutalarm/AlarmReceiver.kt
@@ -5,6 +5,29 @@ import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
 
+/**
+ * A BroadcastReceiver that responds to broadcast intents by playing a specific audio file.
+ *
+ * This receiver is designed to be triggered by alarm events or other conditions that broadcast intents
+ * within the application or from the system. Upon receiving an intent, it initializes a MediaPlayer
+ * instance to play an audio file from the application's raw resources directory.
+ *
+ * The audio playback is configured to loop continuously and play at a high volume level to ensure
+ * it captures the user's attention, making it suitable for alarm or notification purposes.
+ *
+ * Usage:
+ * - The AlarmReceiver must be registered in the AndroidManifest.xml file to respond to specific intents.
+ * - Ensure the audio file `R.raw.brutalshortsound1` exists in the `res/raw` directory.
+ *
+ * Example AndroidManifest.xml declaration:
+ * `<receiver android:name=".AlarmReceiver"/>`
+ *
+ * Note:
+ * - Context provided to `onReceive` may be used to safely access system services and application-specific resources.
+ * - The intent parameter can be used to pass additional data to this receiver when broadcasting the intent.
+ */
+
+
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         val mediaPlayer: MediaPlayer? = MediaPlayer.create(context, R.raw.brutalshortsound1)
